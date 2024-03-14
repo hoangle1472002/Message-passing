0ab53f57e136a21fbdbdadaf81f926390928302d
hoangle1472002 <hoangle14702@gmail.com>
2024-03-15 00:05:17 +0700

    update kafka

--- modules/person_service/wsgi.py
+++ modules/person_service/wsgi.py
@@ -5,9 +5,9 @@ from app import create_app
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
-    # TOPIC_NAME = 'items'
-    # KAFKA_SERVER = 'localhost:9092'
-    # producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-    # producer.send(TOPIC_NAME, b'Test Message!!!')
-    # producer.flush()
+    TOPIC_NAME = 'items'
+    KAFKA_SERVER = 'localhost:9092'
+    producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+    producer.send(TOPIC_NAME, b'Test Message!!!')
+    producer.flush()
     app.run(debug=True)
