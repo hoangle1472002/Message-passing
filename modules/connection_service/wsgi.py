e4e9a3f4a4c22b295d26266ba624a921a09df17f
hoangle1472002 <hoangle14702@gmail.com>
2024-03-15 00:00:14 +0700

    update config and add docs folder

--- modules/connection_service/wsgi.py
+++ modules/connection_service/wsgi.py
@@ -5,8 +5,8 @@ from app import create_app
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
-    TOPIC_NAME = 'items'
-    consumer = KafkaConsumer(TOPIC_NAME)
-    for message in consumer:
-        print (message)
+    # TOPIC_NAME = 'items'
+    # consumer = KafkaConsumer(TOPIC_NAME)
+    # for message in consumer:
+    #     print (message)
     app.run(debug=True)
